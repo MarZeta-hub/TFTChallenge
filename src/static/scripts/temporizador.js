01dc19cfaa50cd07b6c059bc19239d6c45c8ac8b
Marce <100383266@alumnos.uc3m.es>
2021-04-26 15:49:13 +0200

    Temporizador

---
+++ src/static/scripts/temporizador.js
@@ -0,0 +1,7 @@
+
+$("#temporizador").countdown({
+    date: '04/28/2021 9:00:00',
+    contenedor: "#temporizador"
+    }, function () {
+      alert('¡Empieza la competición: CORRE A JUGAR!');
+    });
